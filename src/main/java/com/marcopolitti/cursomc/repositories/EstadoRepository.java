484d8289ece3fca72fa3212d09feeaa8b08677e6
Marco Politti <marcopolitti.br@gmail.com>
2018-11-29 06:23:31 -0200

    Estado e Cidade

---
+++ src/main/java/com/marcopolitti/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.marcopolitti.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcopolitti.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
